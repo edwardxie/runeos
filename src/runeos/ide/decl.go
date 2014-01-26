f6d8631581e8615a8d27ba8247687c04c0fc49f1
Edward Xie <hzedward.xie@gmail.com>
2014-01-26 19:21:35 +0800

    Add ide package decl.go

---
+++ src/runeos/ide/decl.go
@@ -0,0 +1,86 @@
+package ide
+
+// Environment type declaration
+type Enver interface{}
+
+type Env struct {
+	Os       string `json:"Os"`
+	Language string `json:"Language"`
+	Arch     string `json:"Arch"`
+
+	// Sysenv []string
+	Sdk
+	Goenv
+	Cenv
+	Cppenv
+	Luaenv
+	Javaenv
+	Nodejsenv
+	Pythonenv
+	Rubyenv
+	Bashenv
+}
+
+// type Sysenv struct {
+//  Sysenv []string
+// }
+
+type Sdk struct{}
+
+type Goenv struct {
+	Goroot string `json:"GOROOT",xml:"GOROOT`
+	Gopath string `json:"GOPATH"`
+}
+
+type Cenv struct{}
+
+type Cppenv struct{}
+
+type Luaenv struct{}
+
+type Javaenv struct{}
+
+type Nodejsenv struct{}
+
+type Pythonenv struct{}
+
+type Rubyenv struct{}
+
+type Bashenv struct{}
+
+type Webenv struct {
+	Address string
+	Port    string
+}
+
+// Workspace type declaration
+type Workspace struct {
+	Env      `json:"Env"`
+	Setting  `json:"Setting"`
+	Projects []Project `json:"Projects"`
+}
+
+type Setting struct {
+	Editor  `json:"Editor"`
+	KeyMaps `json:"KeyMaps"`
+}
+
+type Editor struct {
+	FontFace    string
+	FontSize    int
+	TabSize     int
+	Theme       string
+	ColorScheme string
+
+	LineNumber bool
+}
+
+type KeyMaps struct{}
+
+// Project type declaration
+type Project struct {
+	Path     string
+	FileList string
+	FileType string
+	LibList  string
+}
